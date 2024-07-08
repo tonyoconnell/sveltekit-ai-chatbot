6c35ffbceca4a9e39044b8c4d76549ef2edea1a1
Anthony O'Connell <tony@one.ie>
2024-07-08 14:48:17 +0700

    Working

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
 	},
 	preprocess: vitePreprocess()
 };
